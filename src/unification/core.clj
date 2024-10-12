9fc04209c746296604451c80da32b004038aad51
Johnny Martinez <johnnymn@hotmail.com>
2024-10-12 15:02:23 -0600

    Implement the unification

--- src/unification/core.clj
+++ src/unification/core.clj
@@ -1,6 +1,67 @@
-(ns unification.core)
+(ns unification.core
+  (:require [unification.expressions :as e]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare unify)
+
+(defn unify-var
+  "Attempts to unify a variable with a given expression.
+  
+  Params:
+  - var: the variable to unify
+  - exp: the expression to unify
+  - subs: a substitutions map
+  
+  Returns: the updated subs or a :failure"
+
+  [var exp subs]
+  (let [var-val (get subs var)]
+    (cond
+      ;; The variable is already bound to a value in the
+      ;; substitution map so unify the value that the
+      ;; variable is bound to with the new expression.
+      var-val (unify var-val exp subs)
+
+      ;; The expression is also a variable so check if it's 
+      ;; already bound to a value in the substitution map. If
+      ;; it is, recursively unify, if not, bind it. 
+      (e/variable? exp) (if-let [exp-val (get subs exp)]
+                          (unify var exp-val subs)
+                          (assoc subs var exp))
+
+      ;; The variable has not been bound and the expression is not a
+      ;; variable so bound the variable to the expression.
+      :else (assoc subs var exp))))
+
+(defn unify
+  "Attempts to unify two expressions using a substitution map.
+  
+  Params:
+  - a: first expression
+  - b: second expression
+  - subs: a substitutions map
+  
+  Returns: the updated subs or a :failure"
+  [a b subs]
+  (cond
+    ;; Base case: the unification failed already.
+    (= subs :failure) :failure
+
+    ;; If a and b are equal after we substitute 
+    ;; both then return the subs map.
+    (= (e/substitute a subs) (e/substitute b subs)) subs
+
+    ;; If a is a variable then unify it with b.
+    (e/variable? a) (unify-var a b subs)
+
+    ;; Same thing but for b.
+    (e/variable? b) (unify-var b a subs)
+
+    ;; If a and b are sequences and they have the same
+    ;; length then unify the pairs of expressions
+    ;; formed from them.
+    (and (sequential? a) (sequential? b)
+         (= (count a) (count b)))
+    (reduce (fn [s [a b]] (unify a b s)) subs (map vector a b))
+
+    :else :failure))
+
--- test/unification/core_test.clj
+++ test/unification/core_test.clj
@@ -1,7 +1,42 @@
 (ns unification.core-test
-  (:require [clojure.test :refer :all]
-            [unification.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [unification.core :refer [unify-var unify]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-unify-var
+  ;; Test with a variable already bound in substitution map.
+  (is (= (unify-var '?x 'alice {'?x 'bob}) :failure))
+
+  ;; Test that a variable can unify with an unbound value.
+  (is (= (unify-var '?x 'alice {}) {'?x 'alice}))
+
+  ;; Test when both expressions are variables, one is bound.
+  (is (= (unify-var '?x '?y {'?y 'pizza}) {'?x 'pizza, '?y 'pizza}))
+
+  ;; Test when both variables are unbound.
+  (is (= (unify-var '?x '?y {}) {'?x '?y}))
+
+  ;; Test that a non-variable expression binds the variable.
+  (is (= (unify-var '?x 'pizza {}) {'?x 'pizza})))
+
+(deftest test-unify
+  ;; Test with the same expressions.
+  (is (= (unify 'alice 'alice {}) {}))
+
+  ;; Test unifying variables with constants.
+  (is (= (unify '?who 'alice {}) {'?who 'alice}))
+
+  ;; Test unifying two variables.
+  (is (= (unify '?who '?what {}) {'?who '?what}))
+
+  ;; Test unifying sequences of the same length.
+  (is (= (unify '(likes ?who pizza) '(likes alice pizza) {})
+         {'?who 'alice}))
+
+  ;; Test that unification fails when sequence lengths don't match.
+  (is (= (unify '(likes ?who pizza) '(likes alice) {}) :failure))
+
+  ;; Test unifying nested sequences.
+  (is (= (unify '(and (likes ?who pizza) (likes ?who ice-cream))
+                '(and (likes alice pizza) (likes alice ice-cream)) {})
+         {'?who 'alice})))
+
